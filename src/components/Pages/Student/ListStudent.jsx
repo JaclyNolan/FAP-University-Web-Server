e4603326f417738fe87a13b4a8115d3c63fdb71d
gudboi <khiemdv1@fsoft.com>
2023-06-06 22:15:11 +0700

    testtt

--- src/components/Pages/Student/ListStudent.jsx
+++ src/components/Pages/Student/ListStudent.jsx
@@ -7,7 +7,7 @@ const ListStudent = () => {
     const {Search} = Input
 
     const handleChange = () => {
-        console.log('changingg');
+        console.log('changinggg');
     }
     const onSearch = () => {
 
