3d05bfe19efe5f94f5d228674320c209253aed2e
Reew52 <112863268+Reew52@users.noreply.github.com>
2023-06-26 18:23:32 +0700

    Update List.jsx

--- src/components/Pages/User/List.jsx
+++ src/components/Pages/User/List.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import classes from '../Page.module.scss'
-import { Select, Input , Button, Popconfirm, Tag, Table} from 'antd'
-import Link from 'antd/es/typography/Link'
-import Image from '../../common/Image/Image'
+import React, { useState, useEffect } from 'react';
+import classes from '../Page.module.scss';
+import { Select, Input, Button, Popconfirm, Tag, Table, Spin, Alert } from 'antd';
+import Link from 'antd/es/typography/Link';
+import Image from '../../common/Image/Image';
+import axios from 'axios';
+
 const List = () => {
-    const {Search} = Input
+    const { Search } = Input
+
+    const [userData, setUserData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetchUserData();
+    }, []);
+
+    const fetchUserData = async () => {
+        setIsLoading(true);
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/api/users', {
+                headers: {
+                    Authorization: 'Bearer 4|7HBDxuzXMwqobkHDqzN16dhsoJ3PcK8hC9NJpJWa',
+                },
+            });
+            const { status, message, users } = response.data;
+
+            if (status === 200) {
+                setUserData(users);
+            } else {
+                setError(message);
+            }
+        } catch (error) {
+            setError('Error fetching user data');
+        }
+        setIsLoading(false);
+    };
 
     const handleChange = () => {
 
@@ -17,7 +48,7 @@ const List = () => {
     }
     const findTagColor = (role) => {
         let color = ''
-        switch(role){
+        switch (role) {
             case 'Admin':
                 color = 'red'
                 break
@@ -44,7 +75,7 @@ const List = () => {
             title: 'Image',
             dataIndex: 'image',
             key: 'image',
-            render: (text) => <Image src={text.src} alt={text.alt} width = {30} height = {30}/>
+            render: (text) => <Image src={text.src} alt={text.alt} width={30} height={30} />
         },
         {
             title: 'User Name',
@@ -94,277 +125,46 @@ const List = () => {
         },
 
     ]
-    const tableData = [
-        {
-            key: '1',
-            image: {
-                src: 'https://img.freepik.com/free-icon/user_318-159711.jpg',
-                alt: 'user'
-            },
-            username: {
-                text: 'Nguyen Van A',
-                id: 1
-            },
-            email: 'anvbhaf190345@fpt.edu.vn',
-            role: {
-                text: 'Admin',
-                role: 'Admin'
-            },
-            detail: {
-                id: 1,
-                text: 'Details'
-            },
-            actions: {
-                id: 1
-            }
-        },
-        {
-            key: '2',
-            image: {
-                src: 'https://img.freepik.com/free-icon/user_318-159711.jpg',
-                alt: 'user'
-            },
-            username: {
-                text: 'Nguyen Van B',
-                id: 2
-            },
-            email: 'bnvbhaf190345@fpt.edu.vn',
-            role: {
-                text: 'student',
-                role: 'student'
-            },
-            detail: {
-                id: 2,
-                text: 'Details'
-            },
-            actions: {
-                id: 2
-            }
-        },
-        {
-            key: '3',
-            image: {
-                src: 'https://img.freepik.com/free-icon/user_318-159711.jpg',
-                alt: 'user'
-            },
-            username: {
-                text: 'Nguyen Van C',
-                id: 3
-            },
-            email: 'cnvbhaf190345@fpt.edu.vn',
-            role: {
-                text: 'teacher',
-                role: 'teacher'
-            },
-            detail: {
-                id: 3,
-                text: 'Details'
-            },
-            actions: {
-                id: 3
-            }
-        },
-        {
-            key: '4',
-            image: {
-                src: 'https://img.freepik.com/free-icon/user_318-159711.jpg',
-                alt: 'user'
-            },
-            username: {
-                text: 'Nguyen Van D',
-                id: 4
-            },
-            email: 'dnvbhaf190345@fpt.edu.vn',
-            role: {
-                text: 'staff',
-                role: 'staff'
-            },
-            detail: {
-                id: 4,
-                text: 'Details'
-            },
-            actions: {
-                id:4
-            }
-        },
-        {
-            key: '5',
-            image: {
-                src: 'https://img.freepik.com/free-icon/user_318-159711.jpg',
-                alt: 'user'
-            },
-            username: {
-                text: 'Nguyen Van D',
-                id: 4
-            },
-            email: 'dnvbhaf190345@fpt.edu.vn',
-            role: {
-                text: 'staff',
-                role: 'staff'
-            },
-            detail: {
-                id: 4,
-                text: 'Details'
-            },
-            actions: {
-                id:4
-            }
-        },
-        {
-            key: '6',
-            image: {
-                src: 'https://img.freepik.com/free-icon/user_318-159711.jpg',
-                alt: 'user'
-            },
-            username: {
-                text: 'Nguyen Van D',
-                id: 4
-            },
-            email: 'dnvbhaf190345@fpt.edu.vn',
-            role: {
-                text: 'staff',
-                role: 'staff'
-            },
-            detail: {
-                id: 4,
-                text: 'Details'
-            },
-            actions: {
-                id:4
-            }
-        },
-        {
-            key: '7',
-            image: {
-                src: 'https://img.freepik.com/free-icon/user_318-159711.jpg',
-                alt: 'user'
-            },
-            username: {
-                text: 'Nguyen Van D',
-                id: 4
-            },
-            email: 'dnvbhaf190345@fpt.edu.vn',
-            role: {
-                text: 'staff',
-                role: 'staff'
-            },
-            detail: {
-                id: 4,
-                text: 'Details'
-            },
-            actions: {
-                id:4
-            }
-        },
-        {
-            key: '8',
-            image: {
-                src: 'https://img.freepik.com/free-icon/user_318-159711.jpg',
-                alt: 'user'
-            },
-            username: {
-                text: 'Nguyen Van D',
-                id: 4
-            },
-            email: 'dnvbhaf190345@fpt.edu.vn',
-            role: {
-                text: 'staff',
-                role: 'staff'
-            },
-            detail: {
-                id: 4,
-                text: 'Details'
-            },
-            actions: {
-                id:4
-            }
-        },
-        {
-            key: '9',
-            image: {
-                src: 'https://img.freepik.com/free-icon/user_318-159711.jpg',
-                alt: 'user'
-            },
-            username: {
-                text: 'Nguyen Van D',
-                id: 4
-            },
-            email: 'dnvbhaf190345@fpt.edu.vn',
-            role: {
-                text: 'staff',
-                role: 'staff'
-            },
-            detail: {
-                id: 4,
-                text: 'Details'
-            },
-            actions: {
-                id:4
-            }
-        },
-        {
-            key: '10',
-            image: {
-                src: 'https://img.freepik.com/free-icon/user_318-159711.jpg',
-                alt: 'user'
-            },
-            username: {
-                text: 'Nguyen Van D',
-                id: 4
-            },
-            email: 'dnvbhaf190345@fpt.edu.vn',
-            role: {
-                text: 'staff',
-                role: 'staff'
-            },
-            detail: {
-                id: 4,
-                text: 'Details'
-            },
-            actions: {
-                id:4
-            }
-        }
-    ]
-  return (
-    <div className={classes['list']}>
-        <p className={classes['page__title']}>User List</p>
-        <div className={classes['list__main']}>
-            <div className={classes['list__nav']}>
-                <div className={classes['list__nav-left']}>
-                <Select
-                    defaultValue="All"
-                    style={{ width: 120 }}
-                    onChange={handleChange}
-                    options={[
-                        { value: 'Admin', label: 'Admin' },
-                        { value: 'Staff', label: 'Staff' },
-                        { value: 'Student', label: 'Student' },
-                        { value: 'Teacher', label: 'Teacher' },
-                        // { value: 'disabled', label: 'Disabled', disabled: true },
-                    ]}
-                    />
-                </div>
-                <div className={classes['list__nav-right']}>
-                    <div className={classes['list__nav-right__search']}>
-                        <Search placeholder="input search text" onSearch={onSearch} style={{ width: 200 }} />
+
+    return (
+        <div className={classes['list']}>
+            <p className={classes['page__title']}>User List</p>
+            <div className={classes['list__main']}>
+                <div className={classes['list__nav']}>
+                    <div className={classes['list__nav-left']}>
+                        <Select
+                            defaultValue="All"
+                            style={{ width: 120 }}
+                            onChange={handleChange}
+                            options={[
+                                { value: 'Admin', label: 'Admin' },
+                                { value: 'Staff', label: 'Staff' },
+                                { value: 'Student', label: 'Student' },
+                                { value: 'Teacher', label: 'Teacher' },
+                                // { value: 'disabled', label: 'Disabled', disabled: true },
+                            ]}
+                        />
                     </div>
-                    <div className={classes['list__nav-right__add']}>
-                        <Link href='/user/add'>
-                            <Button type='primary'>
-                                <i className="fas fa-plus"></i>
-                                <span>Add</span>
-                            </Button>
-                        </Link>
+                    <div className={classes['list__nav-right']}>
+                        <div className={classes['list__nav-right__search']}>
+                            <Search placeholder="input search text" onSearch={onSearch} style={{ width: 200 }} />
+                        </div>
+                        <div className={classes['list__nav-right__add']}>
+                            <Link href='/user/add'>
+                                <Button type='primary'>
+                                    <i className="fas fa-plus"></i>
+                                    <span>Add</span>
+                                </Button>
+                            </Link>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className={classes['list__table']}>
-                <Table columns={tableColumns} pagination={{ pageSize: 6 }} dataSource={tableData} />
+                <div className={classes['list__table']}>
+                    <Table columns={tableColumns} pagination={{ pageSize: 6 }} dataSource={tableData} />
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default List
