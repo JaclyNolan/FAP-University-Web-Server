20d519bb75e4a9b83d8e94d3fd73bca447b562c7
Reew52 <112863268+Reew52@users.noreply.github.com>
2023-07-06 01:21:19 +0700

    update feedbackList and GradeList

--- src/components/Pages/Grade/GradeList.jsx
+++ src/components/Pages/Grade/GradeList.jsx
@@ -1,19 +1,184 @@
-import React from 'react'
-import {Input, Button, Popconfirm, Select, Table} from 'antd'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect, useRef, useContext, useMemo } from 'react';
+import { Input, Button, Popconfirm, Select, Table, Alert, Space } from 'antd'
 import classes from '../Page.module.scss'
+import axiosClient from '../../../axios-client';
+import { Link, useLocation } from 'react-router-dom';
+import debounce from 'lodash/debounce';
+import { LoadingOutlined, SearchOutlined } from '@ant-design/icons';
+import DebounceSelect from '../../../helpers/customs/DebounceSelect';
+import ContentContext from '../../../helpers/Context/ContentContext';
+
 const GradeList = () => {
-    const {Search} = Input
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search)
 
-    const handleChange = () => {
-        console.log('changinggg');
-    }
-    const onSearch = () => {
+    const [tableData, setTableData] = useState([]);
+    const [totalPages, setTotalPages] = useState(1);
+    const [isFetching, setFetching] = useState(false);
+    const fetchRef = useRef(0);
+    const { setContentLoading } = useContext(ContentContext)
 
+    const [currentPage, setCurrentPage] = useState(1);
+    const [search, setSearch] = useState(searchParams.get('search') ? searchParams.get('search') : '');
+    const [courseId, setCourseId] = useState(searchParams.get('course') ? searchParams.get('course') : 'all');
+    const [classId, setClassId] = useState(searchParams.get('class') ? searchParams.get('class') : 'all');
+
+    const [errorMessage, _setErrorMessage] = useState("");
+    const [successMessage, _setSuccessMessage] = useState("");
+
+    const setErrorMessage = (value) => {
+        _setErrorMessage(value);
+        _setSuccessMessage("");
     }
-    const deleteUserHandler = (id) => {
-        alert(`Deleted ${id}`)
+    const setSuccessMessage = (value) => {
+        _setErrorMessage("");
+        _setSuccessMessage(value);
     }
+
+    const statusLabels = {
+        0: 'F',
+        1: 'P',
+        2: 'M',
+        3: 'D',
+      };
+
+    const getTableDataFromGradeData = (gradeData) => {
+        console.log(gradeData);
+        return gradeData.map((grade) => ({
+            key: grade.id,
+            student_id: grade.student_id,
+            student_name: grade.student_name,
+            class_id: grade.class_id,
+            class_name: grade.class_name,
+            course_id: grade.course_id,
+            course_name: grade.course_name,
+            status: grade.status,
+            score: grade.score,
+            actions: {
+                id: grade.id
+            }
+        }))
+    }
+
+    async function fetchCourseList(searchValue) {
+        setContentLoading(true);
+        const url = '/courses'
+            + (searchValue !== "" ? `?page=1&keyword=${searchValue}` : '')
+        console.log(url);
+
+        try {
+            const response = await axiosClient.get(url)
+            // console.log(response)
+            const data = response.data.courses
+            // console.log(data);
+            setContentLoading(false);
+            return data.map((info) => ({
+                label: `${info.id} ${info.name}`,
+                value: info.id,
+            }))
+        } catch (error) {
+            console.log(error)
+            setErrorMessage(error.response.data.message);
+            setContentLoading(false);
+        }
+    }
+    async function fetchClassList(searchValue) {
+        setContentLoading(true);
+        const url = '/class'
+            + (searchValue !== "" ? `?page=1&keyword=${searchValue}` : '')
+        console.log(url);
+
+        try {
+            const response = await axiosClient.get(url)
+            // console.log(response)
+            const data = response.data.classes
+            // console.log(data);
+            setContentLoading(false);
+            return data.map((info) => ({
+                label: `${info.id} ${info.name}`,
+                value: info.id,
+            }))
+        } catch (error) {
+            console.log(error)
+            setErrorMessage(error.response.data.message);
+            setContentLoading(false);
+        }
+    }
+
+    const fetchClassCourseData = async (currentPage, search, courseId, classId) => {
+        setFetching(true);
+        const url = `/grades?page=${currentPage}`
+            + (courseId !== 'all' ? `&course=${courseId}` : '')
+            + (classId !== 'all' ? `&class=${classId}` : '')
+            + (search !== "" ? `&keyword=${search}` : ``);
+        console.log(url);
+        fetchRef.current += 1;
+        const fetchId = fetchRef.current;
+        await axiosClient.get(url)
+            .then((response) => {
+                if (fetchId !== fetchRef.current) return
+                const { grades, total_pages } = response.data;
+                setTotalPages(total_pages);
+                setTableData(getTableDataFromGradeData(grades));
+                setFetching(false);
+            })
+            .catch((error) => {
+                console.log(error);
+                setErrorMessage(error.message);
+                setFetching(false);
+            })
+    }
+
+    useEffect(() => {
+        fetchClassCourseData(currentPage, search, courseId, classId)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentPage, search, courseId, classId]);
+
+    const handleCourseChange = (value) => {
+        setCourseId(value);
+        setCurrentPage(1);
+    }
+
+    const handleClassChange = (value) => {
+        setClassId(value);
+        setCurrentPage(1);
+    }
+
+    const debounceSetter = useMemo(() => {
+        const handleSearch = (e) => {
+            setSearch(e.target.value);
+            setCurrentPage(1);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        return debounce(handleSearch, 700);
+    })
+
+    const handlePageChange = (page, pageSize) => {
+        setCurrentPage(page);
+    }
+
+    const deleteClassCourseHandler = (id) => {
+        (async () => {
+            setFetching(true);
+            await axiosClient.put(`/classCourses/delete-classCourse/${id}`)
+                .then((response) => {
+                    setFetching(false);
+                    setSuccessMessage('Successfully delete user with id ' + id)
+                })
+                .catch((error) => {
+                    setFetching(false);
+                    setErrorMessage(error.message);
+                })
+            if (!errorMessage) {
+                if (currentPage === 1) {
+                    fetchClassCourseData(currentPage, search, courseId, classId)
+                } else {
+                    setCurrentPage(1);
+                }
+            }
+        })()
+    }
+
     const tableColumns = [
         {
             title: 'Id',
@@ -21,24 +186,30 @@ const GradeList = () => {
             key: 'id',
         },
         {
+            title: 'Student ID',
+            dataIndex: 'student_id',
+            key: 'student_id'
+        },
+        {
             title: 'Student Name',
-            dataIndex: 'studentName',
-            key: 'studentName'
+            dataIndex: 'student_name',
+            key: 'student_name'
         },
         {
             title: 'Class',
-            dataIndex: 'class',
-            key: 'class',
+            dataIndex: 'class_name',
+            key: 'class_name',
         },
         {
             title: 'Course',
-            dataIndex: 'course',
-            key: 'course',
+            dataIndex: 'course_name',
+            key: 'course_name',
         },
         {
             title: 'Status',
             dataIndex: 'status',
-            key: 'status'
+            key: 'status',
+            render: (status) => statusLabels[status] || '', // Sử dụng đối tượng
         },
         {
             title: 'score',
@@ -70,69 +241,86 @@ const GradeList = () => {
         },
 
     ]
-    const tableData = [
-        {
-            key: '1',
-            studentName: 'Nguyen Van A',
-            class: 'BHAF123',
-            course: 'Programing',
-            status: 'in progress',
-            address: 'Ha Noi - Viet Nam',
-            actions: {
-                id: 1
-            }
-        }
-    ]
-  return (
-    <div className={classes['list']}>
-        <p className={classes['page__title']}>Grade List</p>
-        <div className={classes['list__main']}>
-            <div className={classes['list__nav']}>
-                <div className={classes['list__nav-left']}>
-                    
-                </div>
-                <div className={classes['list__nav-right']}>
-                    <div className={classes['list__nav-right__search']}>
-                        <Search placeholder="input search text" onSearch={onSearch} style={{ width: 200 }} />
+    // const tableData = [
+    //     {
+    //         key: '1',
+    //         studentName: 'Nguyen Van A',
+    //         class: 'BHAF123',
+    //         course: 'Programing',
+    //         status: 'in progress',
+    //         address: 'Ha Noi - Viet Nam',
+    //         actions: {
+    //             id: 1
+    //         }
+    //     }
+    // ]
+    return (
+        <div className={classes['list']}>
+            {successMessage !== "" && <Alert type='success' banner message={successMessage} />}
+            {errorMessage !== "" && <Alert type='error' banner message={errorMessage} />}
+            <p className={classes['page__title']}>Grade List</p>
+            <div className={classes['list__main']}>
+                <div className={classes['list__nav']}>
+                    <div className={classes['list__nav-left']}>
+
                     </div>
-                    <div className={classes['list__nav-right__add']}>
-                        <Link to='/grade/add'>
-                            <Button type='primary'>
-                                <i className="fas fa-plus"></i>
-                                <span>Add</span>
-                            </Button>
-                        </Link>
+                    <div className={classes['list__nav-right']}>
+                        <div className={classes['list__nav-right__search']}>
+                            <Input
+                                prefix={isFetching ? <LoadingOutlined /> : <SearchOutlined />}
+                                placeholder="input search text"
+                                allowClear
+                                onChange={debounceSetter}
+                                style={{ width: 200 }}
+                            />
+                        </div>
+                        <div className={classes['list__nav-right__add']}>
+                            <Link to='/grade/add'>
+                                <Button type='primary'>
+                                    <i className="fas fa-plus"></i>
+                                    <span>Add</span>
+                                </Button>
+                            </Link>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className={classes['list__filters']}>
-                    <Select
-                        defaultValue="Course"
-                        style={{ width: 120 }}
-                        onChange={handleChange}
-                        options={[
-                            { value: 'Programing', label: 'Programing' },
-                            { value: 'Networking', label: 'Networking' },
-                            { value: 'DSA', label: 'DSA' },
-                            { value: 'Software lifecycle', label: 'Software lifecycle' },
-                        ]}
-                    />
-                    <Select
-                        defaultValue="Class"
-                        style={{ width: 120 }}
-                        onChange={handleChange}
-                        options={[
-                            { value: 'BHAF123', label: 'BHAF123' },
-                            { value: 'BHAF124', label: 'BHAF124' },
-                        ]}
-                    />
-            </div>
-            <div className={classes['list__table']}>
-                <Table columns={tableColumns} pagination={{ pageSize: 6 }} dataSource={tableData} />
+                <div className={classes['list__filters']}>
+                    <Space>
+                        <DebounceSelect
+                            defaultValue={courseId}
+                            key='course'
+                            style={{ width: 200 }}
+                            onChange={handleCourseChange}
+                            fetchOptions={fetchCourseList}
+                            presetOptions={[
+                                { value: 'all', label: 'Course' },
+                            ]}
+                        />
+                        <DebounceSelect
+                            defaultValue={classId}
+                            key='class'
+                            style={{ width: 200 }}
+                            onChange={handleClassChange}
+                            fetchOptions={fetchClassList}
+                            presetOptions={[
+                                { value: 'all', label: 'Class' },
+                            ]}
+                        />
+                    </Space>
+                </div>
+                <div className={classes['list__table']}>
+                    <Table columns={tableColumns} loading={isFetching} pagination={{
+                        current: currentPage,
+                        total: totalPages * tableData.length,
+                        pageSize: tableData.length,
+                        defaultCurrent: 1,
+                        showQuickJumper: true,
+                        onChange: handlePageChange
+                    }} dataSource={tableData} />
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default GradeList
