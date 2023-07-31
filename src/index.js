5e59fc247694620f61a031f8f273d0b2c1ebbc8d
JaclyNolan <anhbg330011@gmail.com>
2023-07-31 11:11:52 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-   <GoogleOAuthProvider clientId='971766812836-re275ffnj3jnf9gcefunt2tavn29on7q.apps.googleusercontent.com'>
+  <GoogleOAuthProvider clientId='971766812836-re275ffnj3jnf9gcefunt2tavn29on7q.apps.googleusercontent.com'>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-   </GoogleOAuthProvider>
-  </React.StrictMode>
+  </GoogleOAuthProvider>
 );
