072bd10b61fec3c98de878adce5973bbdefd4999
JaclyNolan <anhbg330011@gmail.com>
2024-01-19 17:42:49 +0700

    Update config.jsx

--- src/helpers/constants/config.jsx
+++ src/helpers/constants/config.jsx
@@ -1,4 +1,4 @@
 // config.js
-const BACKEND_SERVER_URL = 'http://localhost:80'; // Replace with your actual backend server URL
+const BACKEND_SERVER_URL = import.meta.env.VITE_API_URL || 'http://localhost:80'; // Replace with your actual backend server URL
 
 export default BACKEND_SERVER_URL;
